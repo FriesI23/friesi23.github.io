c5a3c3ced96472e59282028097764abf7d731f62
Fries_I23 <FriesI23@outlook.com>
2024-04-29 18:01:16 +0800

    feat: add dark theme

---
+++ assets/js/custom-head.js
@@ -0,0 +1,24 @@
+// blog (c) by FriesI23
+//
+// blog is licensed under a
+// Creative Commons Attribution-ShareAlike 4.0 International License.
+//
+// You should have received a copy of the license along with this
+// work. If not, see <https://creativecommons.org/licenses/by-sa/4.0/>.
+
+$(function () {
+  $(".theme__toggle").on("click", function () {
+    node1 = document.getElementById("theme_source");
+    node2 = document.getElementById("theme_source_dark");
+    if (node1.getAttribute("rel") == "stylesheet") {
+      node1.setAttribute("rel", "stylesheet alternate");
+      node2.setAttribute("rel", "stylesheet");
+      sessionStorage.setItem("theme", "dark");
+    } else {
+      node2.setAttribute("rel", "stylesheet alternate");
+      node1.setAttribute("rel", "stylesheet");
+      sessionStorage.setItem("theme", "light");
+    }
+    return false;
+  });
+});
